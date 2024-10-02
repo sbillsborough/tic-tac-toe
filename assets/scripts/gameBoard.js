fec8853eafdc75ae5dd0a030bb28b30ecfd693c2
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-10-02 17:30:38 +0100

    Game Board and Styling
    For each element in boardArray, a new div is created using document.createElement(div)
    The classList.add(board-cell) method adds a class to each cell div for CSS styling
    setAttribute(data-index, index) adds a data-index attribute to each div element. To track the position of each cell
    textContent = element sets the content of each cell.
    appendChild adds the div to the boardContainerEl.
    
    Styled the board container with grid
    Flex for the board cells with width/height and colour

--- assets/scripts/gameBoard.js
+++ assets/scripts/gameBoard.js
@@ -1,4 +1,13 @@
 export const gameBoard = (function () {
-  const board = ["", "", "", "", "", "", "", "", ""];
-  console.log(board);
+  const boardArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const boardContainerEl = document.querySelector(".board-container");
+
+  boardArray.forEach((element, index) => {
+    const cellDiv = document.createElement("div");
+    cellDiv.classList.add("board-cell");
+    cellDiv.setAttribute("data-index", index);
+    cellDiv.textContent = element;
+
+    boardContainerEl.appendChild(cellDiv);
+  });
 })();
