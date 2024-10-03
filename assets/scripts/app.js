77f11bcbd9d4f3dbbf01bc524dcefc6533343545
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-10-03 17:30:07 +0100

    click event listener that console logs 'Hi'
    this will add the players X when clicked

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,4 @@
 import { gameBoard } from "./gameBoard.js";
 import { player1 } from "./players.js";
 import { player2 } from "./players.js";
+import { boardCell } from "./gameBoard.js";
--- assets/scripts/gameBoard.js
+++ assets/scripts/gameBoard.js
@@ -1,3 +1,4 @@
+// Draws the game board and adds classes/attributes
 export const gameBoard = (function () {
   const boardArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const boardContainerEl = document.querySelector(".board-container");
@@ -12,3 +13,9 @@ export const gameBoard = (function () {
     boardContainerEl.appendChild(cellDiv);
   });
 })();
+
+// Updates the display
+export const boardCell = document.querySelector(".board-cell");
+document.addEventListener("click", () => {
+  console.log("Hi");
+});
