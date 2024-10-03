8b46a9862621e26f6d9c69834222fae02124a603
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-10-03 17:02:09 +0100

    Events JS file to separate event listeners

--- assets/scripts/gameBoard.js
+++ assets/scripts/gameBoard.js
@@ -2,6 +2,7 @@ export const gameBoard = (function () {
   const boardArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const boardContainerEl = document.querySelector(".board-container");
 
+  // Loop through the boardArray and create a div for each element
   boardArray.forEach((element, index) => {
     const cellDiv = document.createElement("div");
     cellDiv.classList.add("board-cell");
